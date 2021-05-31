83d24b690144c2553de81f4495ede2de07393dcf
MarketingPlus <benmarketingplus@gmail.com>
2021-05-31 11:56:06 +1000

    added stuff for the post request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,8 +26,15 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.post('/', (req, res) => {
-  // create a new category
+
+router.post('/', async (req, res) => {
+  
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
