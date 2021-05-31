1eb89690ead7c1530a2654df9066d886685a1da3
MarketingPlus <benmarketingplus@gmail.com>
2021-05-31 11:55:38 +1000

    added the final part for the get request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,11 +13,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {include: Product });
+      // include: [{ model: Product, through: Category, as: 'product_id' }]
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category by this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 router.post('/', (req, res) => {
   // create a new category
 });
