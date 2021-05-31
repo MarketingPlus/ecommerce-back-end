3ae5f746ca09006faa8a66cd2a65bbbb85dbb9a6
MarketingPlus <benmarketingplus@gmail.com>
2021-05-31 10:38:18 +1000

    products belong to many tags has been added

--- models/index.js
+++ models/index.js
@@ -16,6 +16,13 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  foreignKey: 'product_id',
+ // through: { 
+ //   model: ProductTag,
+ // }
+});
 
 // Tags belongToMany Products (through ProductTag)
 
